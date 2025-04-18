94bf951ac63d1f549bccfd6b5f799430ff1cecb9
Abnormal <86753001+AbnormalPoof@users.noreply.github.com>
2025-04-18 14:01:17 -0500

    more wip bs

--- source/funkin/ui/freeplay/dj/ScriptedFreeplayDJ.hx
+++ source/funkin/ui/freeplay/dj/ScriptedFreeplayDJ.hx
@@ -1,8 +1,25 @@
 package funkin.play.ui.freeplay.dj;
 
 /**
- * A script that can be tied to a FreeplayDJ.
+ * A script that can be tied to a ScriptedBaseFreeplayDJ.
+ * Create a scripted class that extends BaseFreeplayDJ to use this.
+ * Note: Making a scripted class extending BaseFreeplayDJ is not recommended.
+ * Do so ONLY if are handling all the rendering yourself,
+ * and can't use one of the built-in render modes.
+ */
+@:hscriptClass
+class ScriptedBaseFreeplayDJ extends BaseFreeplayDJ implements polymod.hscript.HScriptedClass {}
+
+/**
+ * A script that can be tied to a AnimateAtlasFreeplayDJ.
  * Create a scripted class that extends FreeplayDJ to use this.
  */
 @:hscriptClass
-class ScriptedFreeplayDJ extends FreeplayDJ implements polymod.hscript.HScriptedClass {}
+class ScriptedAnimateAtlasFreeplayDJ extends AnimateAtlasFreeplayDJ implements polymod.hscript.HScriptedClass {}
+
+/**
+ * A script that can be tied to a SparrowFreeplayDJ.
+ * Create a scripted class that extends FreeplayDJ to use this.
+ */
+@:hscriptClass
+class ScriptedSparrowFreeplayDJ extends SparrowFreeplayDJ implements polymod.hscript.HScriptedClass {}
