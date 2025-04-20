78b37597bcd7514efe48d81bf72f7f7c8ae2d8ca
Abnormal <86753001+AbnormalPoof@users.noreply.github.com>
2025-04-20 01:52:43 -0500

    scripted freeplay and title

---
+++ source/funkin/ui/title/ScriptedTitleState.hx
@@ -0,0 +1,8 @@
+package funkin.ui.title;
+
+/**
+ * A script that can be tied to TitleState.
+ * Create a scripted class that extends TitleState to use this.
+ */
+@:hscriptClass
+class ScriptedTitleState extends funkin.ui.title.TitleState implements polymod.hscript.HScriptedClass {}
